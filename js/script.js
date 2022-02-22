f5a7490220eba23765ebd579733a0447a4ffe958
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-02-22 14:52:38 +0100

    message display and different operators in if

--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@ const passengerAge = parseInt(prompt("What is your age?"));
 
 let ticketCost = 0.21 * kmToGo;
 
-if (passengerAge <= 18) {
+if (passengerAge < 18) {
   ticketCost = ticketCost - ticketCost * 0.3; // 30% discount
-} else if (passengerAge >= 65) {
+  document.getElementById("message").innerHTML =
+    "30% discount applied because you are underage";
+} else if (passengerAge > 65) {
   ticketCost = ticketCost - ticketCost * 0.65; // 65% discount
+  document.getElementById("message").innerHTML =
+    "65% discount applied because you are reeeeeeally old 😜";
 }
 
 ticketCost = ticketCost.toFixed(2); // 2 decimals
