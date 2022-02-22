b939d26301ef41f14b9ebc6e4cbd292438e274db
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-02-22 14:55:05 +0100

    message done with a variable

--- js/script.js
+++ js/script.js
@@ -2,15 +2,14 @@ const kmToGo = parseInt(prompt("How many km do you have to travel?"));
 const passengerAge = parseInt(prompt("What is your age?"));
 
 let ticketCost = 0.21 * kmToGo;
+let message = "";
 
 if (passengerAge < 18) {
   ticketCost = ticketCost - ticketCost * 0.3; // 30% discount
-  document.getElementById("message").innerHTML =
-    "30% discount applied because you are underage";
+  message = "30% discount applied because you are underage";
 } else if (passengerAge > 65) {
   ticketCost = ticketCost - ticketCost * 0.65; // 65% discount
-  document.getElementById("message").innerHTML =
-    "65% discount applied because you are reeeeeeally old 😜";
+  message = "65% discount applied because you are reeeeeeally old 😜";
 }
 
 ticketCost = ticketCost.toFixed(2); // 2 decimals
@@ -18,3 +17,4 @@ ticketCost = ticketCost.toFixed(2); // 2 decimals
 document.getElementById("kmtogo").innerHTML = kmToGo;
 document.getElementById("passengerage").innerHTML = passengerAge;
 document.getElementById("price").innerHTML = ticketCost;
+document.getElementById("message").innerHTML = message;
